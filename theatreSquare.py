1a2bc365a58dec40463201acbe8401ada735dc38
Adrish Maity <adrish.maity@outlook.com>
2019-06-06 12:23:30 +0530

    completed the code.

--- theatreSquare.py
+++ theatreSquare.py
@@ -1,6 +1,16 @@
-def main():
-    print("working")
+import math
 
+def main():
+
+    n,m,a = map(int,input().split(' '))
+
+    # along n number of a is
+    x = math.ceil(n/a)
+
+    # along n number of a is
+    y = math.ceil(m/a)
+
+    print(x*y)
 
 if __name__ == "__main__":
     main()
